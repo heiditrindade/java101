cf1819c19859c26281971fd0ff35c7bf957e20fe
Heidi Trindade <heidit@tuta.io>
2022-05-22 19:32:35 -0300

    Add increase salary exercise

---
+++ increaseSalary/src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.Scanner;
+import entities.Employee;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Employee employee = new Employee();
+
+        System.out.println("Name: ");
+        employee.name = sc.nextLine();
+        System.out.print("Gross salary: ");
+        employee.grossSalary = sc.nextDouble();
+        System.out.print("Tax: ");
+        employee.tax = sc.nextDouble();
+
+        System.out.println();
+        System.out.println("Employee: " + employee);
+        System.out.println();
+        System.out.println("Which percentage to increase salary? ");
+        double percentage = sc.nextDouble();
+        employee.increaseSalary(percentage);
+
+        System.out.println();
+        System.out.println("Updated data: " + employee);
+        sc.close();
+    }
+}
